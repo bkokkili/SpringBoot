f939973b49278ac9698652a23fbc90c57bd7c032
bkokkili <venkatbalaji504@gmail>
2024-03-19 22:27:29 +0530

    MovieSearch app react integration

--- MoviesSearchWithReact/src/main/java/com/springdemo/moviessearchwithreact/controller/MovieSearchController.java
+++ MoviesSearchWithReact/src/main/java/com/springdemo/moviessearchwithreact/controller/MovieSearchController.java
@@ -1,25 +1,21 @@
 package com.springdemo.moviessearchwithreact.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.springdemo.moviessearchwithreact.model.Movie;
+import com.springdemo.moviessearchwithreact.service.MovieSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.springdemo.moviessearchwithreact.service.MovieSearchService;
-
-import java.util.List;
 
 @RestController
 public class MovieSearchController {
     @Autowired
     MovieSearchService movieSerachService;
-    @CrossOrigin
-@RequestMapping("/loadMoviesBySearch")
-public String fetchMoviesBySearch(@RequestParam String searchTerm, @RequestParam String apiKey) throws JsonProcessingException {
 
-    System.out.println("******searchString******"+searchTerm.trim()+" ********apiKey******* "+apiKey);
-    return movieSerachService.getClient(searchTerm.trim(),apiKey);
-}
+    @CrossOrigin
+    @RequestMapping("/loadMoviesBySearch")
+    public String fetchMoviesBySearch(@RequestParam String searchTerm, @RequestParam String apiKey) throws JsonProcessingException {
+        return movieSerachService.getClient(searchTerm.trim(), apiKey);
+    }
 }
--- MoviesSearchWithReact/src/main/java/com/springdemo/moviessearchwithreact/service/MovieSearchService.java
+++ MoviesSearchWithReact/src/main/java/com/springdemo/moviessearchwithreact/service/MovieSearchService.java
@@ -1,51 +1,40 @@
 package com.springdemo.moviessearchwithreact.service;
-
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.springdemo.moviessearchwithreact.model.Movie;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 import java.util.Objects;
-
-
 @Service
 public class MovieSearchService {
-    public  String getClient(String searchTerm, String apiKey) throws JsonProcessingException {
-           StringBuilder mdb_url = new StringBuilder();
-           mdb_url.append("https://www.omdbapi.com/?s=");
-           mdb_url.append(searchTerm);
-           mdb_url.append("&apikey=");
-           mdb_url.append(apiKey);
+    public String getClient(String searchTerm, String apiKey) throws JsonProcessingException {
+        StringBuilder mdb_url = new StringBuilder();
+        mdb_url.append("https://www.omdbapi.com/?s=");
+        mdb_url.append(searchTerm);
+        mdb_url.append("&apikey=");
+        mdb_url.append(apiKey);
 
-           JSONArray jsonArray1 = new JSONArray();
-           if (searchTerm.isEmpty()){
-               return jsonArray1.toString();
-           }
-           if (searchTerm.length() > 2){
-               RestTemplate restTemplate = new RestTemplate();
+        JSONArray jsonArray1 = new JSONArray();
+        if (searchTerm.isEmpty()) {
+            return jsonArray1.toString();
+        }
+        if (searchTerm.length() > 2) {
+            RestTemplate restTemplate = new RestTemplate();
 
-               ResponseEntity<String> responseEntity = restTemplate.getForEntity(mdb_url.toString(), String.class);
-               ObjectMapper objectMapper = new ObjectMapper();
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(mdb_url.toString(), String.class);
+            ObjectMapper objectMapper = new ObjectMapper();
 
-               JSONObject jsonObject = new JSONObject(Objects.requireNonNull(responseEntity.getBody()));
-               JSONArray jsonArray = jsonObject.getJSONArray("Search");
-               objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-               return jsonArray.toString();
-           }
+            JSONObject jsonObject = new JSONObject(Objects.requireNonNull(responseEntity.getBody()));
+            JSONArray jsonArray = jsonObject.getJSONArray("Search");
+            objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+            return jsonArray.toString();
+        }
 
-       return jsonArray1.toString();
-
+        return jsonArray1.toString();
 
     }
 }
